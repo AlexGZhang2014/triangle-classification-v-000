72f87344f0f5d7f33df3f337c2753c374229b571
Alex Zhang <alexgzhang92@gmail.com>
2018-05-31 20:38:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+  attr_accessor :first_side, :second_side, :third_side
+  
+  def initialize(first_side, second_side, third_side)
+    @first_side = first_side
+    @second_side = second_side
+    @third_side = third_side
+  end
+  
+  def kind
+    if @first_side <= 0 || @second_side <= 0 || @third_side <= 0
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        error.message
+      end
+    elsif @first_side == @second_side && @second_side == @third_side
+      :equilateral
+    elsif @first_side == @second_side && @second_side != @third_side
+      :isosceles
+    elsif @first_side != @second_side && @second_side == @third_side
+      :isosceles
+    elsif @first_side == @third_side && @first_side != @second_side
+      :isosceles
+    elsif @first_side != @second_side && @second_side != @third_side && @first_side != @third_side
+      :scalene
+    else
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        error.message
+      end
+    end
+  end
+  
+  class TriangleError < StandardError
+    "Your triangle is invalid. Please try again."
+  end
 end
+
+
